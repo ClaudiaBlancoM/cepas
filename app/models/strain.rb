4716d82c1965dee279930503644f40ce5d5ab703
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-13 01:35:46 -0400

    commit pendientes

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,2 +1,8 @@
 class Strain < ApplicationRecord
-end
+    #Validations
+    validates :name, presence: true, uniqueness: true
+  
+    #Associations
+    has_many :wine_strains
+    has_many :wines, through: :wine_strains
+  end
